1e16859e4a31c655b3085fe24d696bcdafed0d4c
DZR-Github <105500267+DZR-Github@users.noreply.github.com>
2023-11-18 21:47:24 +0800

    feat: Inital project, and add auth into the project.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,43 @@
+import { currentUser } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  const result = await fetchPosts(1, 30);
+  console.log(result);
+  return (
+    <main>
+      <div className="head-text">Home</div>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </main>
+  );
+}
