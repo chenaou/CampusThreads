07868189486ce7120f65a0f09c301422ff987e23
chenaou <100351202+chenaou@users.noreply.github.com>
2023-11-21 08:37:01 +0800

    re

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,11 @@
-
+/*
+ * @Author: zrDeng
+ * @Date: 2023-08-29 14:16:14
+ * @LastEditTime: 2023-08-30 09:52:44
+ * @LastEditors: zrDeng
+ * @Description: 
+ * @FilePath: \项目\threads\app\(root)\profile\[id]\page.tsx
+ */
 import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
